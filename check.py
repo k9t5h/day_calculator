04f2c306ab450ba19367304cab4eb5661b331375
Adam Peresztegi <flash42@gmail.com>
2020-07-02 13:57:44 +0200

    Fix check script

--- check.py
+++ check.py
@@ -1,10 +1,20 @@
 from datetime import datetime
 from day_calculator import calculate_day_type
 
-with open("result", "r") as result:
-    for line in result:
+with open("results", "r") as results:
+    error_cnt = 0
+    for line in results:
         day, required = line.split(" ")
         date = datetime.strptime(day, '%Y-%m-%d')
-        calculated = calculate_day_type(date, "b")
-        if required.strip() != calculated.strip():
-            print("Type of day: ", date.date(), "should be: ", required.strip(), "instead it was: ", calculated.strip())
+        calculated = calculate_day_type(date)
+        try:
+            if required.strip() != calculated.strip():
+                print("Type of day: ", date.date(), "should be: ", required.strip(), "instead it was: ", calculated.strip())
+                error_cnt += 1
+        except Exception as e:
+            print(e)
+            error_cnt += 1
+    if error_cnt > 0:
+        print("finished with " + str(error_cnt) + " errors")
+    else:
+        print("SUCCESS")
